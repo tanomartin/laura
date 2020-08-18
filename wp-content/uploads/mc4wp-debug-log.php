5d67ff60eb03db741d15d09f16710a3b87bbe00e
tanomartin <tanomartin05@gmail.com>
2020-08-18 15:28:44 -0300

    merge de elmimnacion de base

---
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -0,0 +1,23 @@
+<?php exit; ?>
+[2020-08-18 14:02:08] WARNING: Form 5 > mmzu********@us****.com.ar is already subscribed to the selected list(s)
+[2020-08-18 14:02:15] WARNING: Form 5 > mmzu********@us****.com.ar is already subscribed to the selected list(s)
+[2020-08-18 14:07:04] ERROR: Form 5 > Mailchimp API error: 400 Bad Request. Forgotten Email Not Subscribed. tano********@ya***.com.ar was permanently deleted and cannot be re-imported. The contact must re-subscribe to get back on the list.
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/afb30592b9/members
+
+{"status":"pending","email_address":"tano********@ya***.com.ar","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"127.0.0.1","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"http://developer.mailchimp.com/documentation/mailchimp/guides/error-glossary/","title":"Forgotten Email Not Subscribed","status":400,"detail":"tano********@ya***.com.ar was permanently deleted and cannot be re-imported. The contact must re-subscribe to get back on the list.","instance":"ea02fe18-5f09-4bb8-93c2-0e3b46cd9849"}
+[2020-08-18 14:13:46] ERROR: Form 5 > Mailchimp API error: 400 Bad Request. Forgotten Email Not Subscribed. tano********@ya***.com.ar was permanently deleted and cannot be re-imported. The contact must re-subscribe to get back on the list.
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/afb30592b9/members
+
+{"status":"pending","email_address":"tano********@ya***.com.ar","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"127.0.0.1","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"http://developer.mailchimp.com/documentation/mailchimp/guides/error-glossary/","title":"Forgotten Email Not Subscribed","status":400,"detail":"tano********@ya***.com.ar was permanently deleted and cannot be re-imported. The contact must re-subscribe to get back on the list.","instance":"6791568d-e59e-41d8-84e1-af3775a3dbcd"}
