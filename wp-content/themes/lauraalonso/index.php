08a725584046bc3c5ebdb7b4a6fa694592951c4c
tanomartin <tanomartin05@gmail.com>
2020-09-20 19:57:40 -0300

    agregado de pop up cookies

--- wp-content/themes/lauraalonso/index.php
+++ wp-content/themes/lauraalonso/index.php
@@ -39,10 +39,16 @@
 <link href="<?php echo THEME_URL ?>/style.css" rel="stylesheet">
 
 
+<script type="text/javascript">
+window.cookieconsent_options = {"message":"Utilizamos cookies propias y de terceros para mejorar la experiencia del usuario a través de su navegación. Si continúas navegando aceptas su uso.","dismiss":"ACEPTAR","theme":"dark-bottom"};
+</script>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
+
 </head>
 
-<body>
 
+<body>
+<?php include("header.php"); ?>
 
 
 <div class="container h-100">
